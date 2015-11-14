d82a9375f2d46053da44b65585d8d149181de1f5
Daniel Chalmers <MoonDPC@gmail.com>
2015-11-14 00:04:20 -0500

    Setup assembly info

---
+++ DesktopWidgets/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿#region
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+namespace DesktopWidgets
+{
+    internal class AssemblyInfo
+    {
+        public static Version Version { get; } = Assembly.GetExecutingAssembly().GetName().Version;
+        public static string Copyright { get; } = GetAssemblyAttribute<AssemblyCopyrightAttribute>(a => a.Copyright);
+        public static string Title { get; } = GetAssemblyAttribute<AssemblyTitleAttribute>(a => a.Title);
+        public static string Description { get; } = GetAssemblyAttribute<AssemblyDescriptionAttribute>(a => a.Description);
+        public static string Guid { get; } = GetAssemblyAttribute<GuidAttribute>(a => a.Value);
+
+        private static string GetAssemblyAttribute<T>(Func<T, string> value)
+            where T : Attribute
+        {
+            var attribute = (T) Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof (T));
+            return value.Invoke(attribute);
+        }
+    }
+}
