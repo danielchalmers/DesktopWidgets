a8e3710617868844f2f1c092d65241b834382b86
Daniel Chalmers <MoonDPC@gmail.com>
2015-11-13 23:52:20 -0500

    Add global error handling

--- DesktopWidgets/App.xaml.cs
+++ DesktopWidgets/App.xaml.cs
@@ -1,17 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Threading;
 
 namespace DesktopWidgets
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        private static bool SuccessfullyLoaded;
+
+        public App()
+        {
+            Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            SuccessfullyLoaded = true;
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            try
+            {
+                base.OnExit(e);
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        private static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var exception = e.Exception.Message;
+            Popup.Show(
+                SuccessfullyLoaded
+                    ? $"An unhandled exception occurred:\n\n{exception}"
+                    : $"A critical exception occurred:\n\n{exception}\n\nApplication will now exit.",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+            if (!SuccessfullyLoaded)
+                Current.Shutdown();
+        }
     }
 }
---
+++ DesktopWidgets/Popup.cs
@@ -0,0 +1,25 @@
+﻿#region
+
+using System.Windows;
+using DesktopWidgets.Properties;
+
+#endregion
+
+namespace DesktopWidgets
+{
+    internal class Popup
+    {
+        public static MessageBoxResult Show(string text, MessageBoxButton button = MessageBoxButton.OK,
+            MessageBoxImage image = MessageBoxImage.Information, MessageBoxResult defaultButton = MessageBoxResult.OK)
+        {
+            try
+            {
+                return MessageBox.Show(text, Resources.AppName, button, image, defaultButton);
+            }
+            catch
+            {
+                return MessageBoxResult.Cancel;
+            }
+        }
+    }
+}
