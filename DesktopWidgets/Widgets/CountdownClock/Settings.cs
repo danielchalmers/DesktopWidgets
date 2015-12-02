f5228a60319defeb24ee98a37933b527b0fb6d9c
Daniel Chalmers <MoonDPC@gmail.com>
2015-12-02 15:34:52 -0500

    Change "Countdown" widget default "TimeFormat"

--- DesktopWidgets/Widgets/CountdownClock/Settings.cs
+++ DesktopWidgets/Widgets/CountdownClock/Settings.cs
@@ -5,6 +5,11 @@ namespace DesktopWidgets.Widgets.CountdownClock
 {
     public class Settings : WidgetClockSettingsBase
     {
+        public Settings()
+        {
+            TimeFormat = "dd:hh:mm:ss";
+        }
+
         public DateTime EndDateTime { get; set; } = DateTime.Now;
     }
 }
