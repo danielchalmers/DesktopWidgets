363a1a16beb64f2a4f63159aefa3f2b956f731c1
Daniel Chalmers <MoonDPC@gmail.com>
2015-12-07 18:29:15 -0500

    Add "Weather" widget show control options

--- DesktopWidgets/Widgets/Weather/Settings.cs
+++ DesktopWidgets/Widgets/Weather/Settings.cs
@@ -8,5 +8,8 @@ namespace DesktopWidgets.Widgets.Weather
         public TemperatureUnitType UnitType { get; set; }
         public TimeSpan RefreshInterval { get; set; } = TimeSpan.FromHours(1);
         public int ZipCode { get; set; }
+        public bool ShowIcon { get; set; } = true;
+        public bool ShowTemperature { get; set; } = true;
+        public bool ShowDescription { get; set; } = true;
     }
 }
