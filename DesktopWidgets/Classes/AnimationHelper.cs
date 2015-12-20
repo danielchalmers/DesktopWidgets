7702147dc204cc5a50748d5bde3ddb8dccff57d9
Daniel Chalmers <MoonDPC@gmail.com>
2015-12-19 19:56:01 -0500

    Add two directions slide animation support

--- DesktopWidgets/Classes/AnimationHelper.cs
+++ DesktopWidgets/Classes/AnimationHelper.cs
@@ -18,6 +18,78 @@ namespace DesktopWidgets.Classes
         {
             var settings = view.Id.GetSettings();
 
+            var twoAnimations = true;
+
+            switch (settings.DockPosition)
+            {
+                default:
+                    view.RenderTransformOrigin = new Point(0.5, 0.5);
+                    twoAnimations = false;
+                    break;
+                case ScreenDockPosition.Left:
+                    switch (settings.DockAlignment)
+                    {
+                        default:
+                            view.RenderTransformOrigin = new Point(0, 0);
+                            twoAnimations = false;
+                            break;
+                        case ScreenDockAlignment.Top:
+                            view.RenderTransformOrigin = new Point(0, 0);
+                            break;
+                        case ScreenDockAlignment.Bottom:
+                            view.RenderTransformOrigin = new Point(0, 1);
+                            break;
+                    }
+                    break;
+                case ScreenDockPosition.Right:
+                    switch (settings.DockAlignment)
+                    {
+                        default:
+                            view.RenderTransformOrigin = new Point(1, 0);
+                            twoAnimations = false;
+                            break;
+                        case ScreenDockAlignment.Top:
+                            view.RenderTransformOrigin = new Point(1, 0);
+                            break;
+                        case ScreenDockAlignment.Bottom:
+                            view.RenderTransformOrigin = new Point(1, 1);
+                            break;
+                    }
+                    break;
+                case ScreenDockPosition.Top:
+                    switch (settings.DockAlignment)
+                    {
+                        default:
+                            view.RenderTransformOrigin = new Point(0, 1);
+                            twoAnimations = false;
+                            break;
+                        case ScreenDockAlignment.Top:
+                            view.RenderTransformOrigin = new Point(0, 0);
+                            break;
+                        case ScreenDockAlignment.Bottom:
+                            view.RenderTransformOrigin = new Point(1, 0);
+                            break;
+                    }
+                    break;
+                case ScreenDockPosition.Bottom:
+                    switch (settings.DockAlignment)
+                    {
+                        default:
+                            view.RenderTransformOrigin = new Point(1, 1);
+                            twoAnimations = false;
+                            break;
+                        case ScreenDockAlignment.Top:
+                            view.RenderTransformOrigin = new Point(0, 1);
+                            break;
+                        case ScreenDockAlignment.Bottom:
+                            view.RenderTransformOrigin = new Point(1, 1);
+                            break;
+                    }
+                    break;
+            }
+
+            var runCount = twoAnimations ? 2 : 1;
+
             Action startAction = delegate
             {
                 if (animationMode == AnimationMode.Show)
@@ -54,26 +126,7 @@ namespace DesktopWidgets.Classes
                         }
                         : null
             };
-            doubleAnimation.Completed += (sender, args) => finishAction();
-
-            switch (settings.DockPosition)
-            {
-                default:
-                    view.RenderTransformOrigin = new Point(0.5, 0.5);
-                    break;
-                case ScreenDockPosition.Left:
-                    view.RenderTransformOrigin = new Point(0, 1);
-                    break;
-                case ScreenDockPosition.Right:
-                    view.RenderTransformOrigin = new Point(1, 0);
-                    break;
-                case ScreenDockPosition.Top:
-                    view.RenderTransformOrigin = new Point(1, 0);
-                    break;
-                case ScreenDockPosition.Bottom:
-                    view.RenderTransformOrigin = new Point(0, 1);
-                    break;
-            }
+            //doubleAnimation.Completed += (sender, args) => finishAction();
 
             // Start animation.
             startAction();
@@ -81,18 +134,34 @@ namespace DesktopWidgets.Classes
             {
                 case AnimationType.Fade:
                     var storyBoard = new Storyboard();
+                    storyBoard.Completed += (sender, args) => finishAction();
                     storyBoard.Children.Add(doubleAnimation);
                     Storyboard.SetTarget(doubleAnimation, view);
                     Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath(UIElement.OpacityProperty));
                     storyBoard.Begin();
                     break;
                 case AnimationType.Slide:
+                    var counter = 0;
+                    doubleAnimation.Completed += delegate
+                    {
+                        counter++;
+                        if (!twoAnimations || counter == 2)
+                            finishAction();
+                    };
                     var trans = new ScaleTransform();
                     view.RenderTransform = trans;
-                    trans.BeginAnimation(
-                        settings.DockPosition.IsVertical()
-                            ? ScaleTransform.ScaleYProperty
-                            : ScaleTransform.ScaleXProperty, doubleAnimation);
+                    if (twoAnimations)
+                    {
+                        trans.BeginAnimation(ScaleTransform.ScaleXProperty, doubleAnimation);
+                        trans.BeginAnimation(ScaleTransform.ScaleYProperty, doubleAnimation);
+                    }
+                    else
+                    {
+                        trans.BeginAnimation(
+                            settings.DockPosition.IsVertical()
+                                ? ScaleTransform.ScaleYProperty
+                                : ScaleTransform.ScaleXProperty, doubleAnimation);
+                    }
                     break;
                 default:
                     finishAction();
