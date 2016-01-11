38722d5f38b28b871428f4b2a9ca186c630d8df3
Daniel Chalmers <MoonDPC@gmail.com>
2016-01-11 18:31:49 -0500

    Fix widget empty screen name error

--- DesktopWidgets/Helpers/StringHelper.cs
+++ DesktopWidgets/Helpers/StringHelper.cs
@@ -6,6 +6,8 @@ namespace DesktopWidgets.Helpers
     {
         public static bool Contains(this string source, string value, bool caseInsensitive)
         {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrEmpty(value))
+                return false;
             return caseInsensitive
                 ? source.IndexOf(value, 0, StringComparison.CurrentCultureIgnoreCase) != -1
                 : source.Contains(value);
