d577e7d388d8dd40ffd63b701654eef54089d04a
Daniel Chalmers <MoonDPC@gmail.com>
2016-01-27 19:18:06 -0500

    Fix NaN floating point comparison

--- DesktopWidgets/Helpers/DoubleHelper.cs
+++ DesktopWidgets/Helpers/DoubleHelper.cs
@@ -6,7 +6,7 @@ namespace DesktopWidgets.Helpers
     public static class DoubleHelper
     {
         public static bool IsEqual(this double val1, double val2) =>
-            double.IsNaN(val1) || double.IsNaN(val1) ||
+            double.IsNaN(val1) || double.IsNaN(val2) ||
             (Math.Abs(val1 - val2) > Settings.Default.DoubleComparisonTolerance);
     }
 }
