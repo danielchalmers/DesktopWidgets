c7ad0a1a228306de0749cd582f4a509a1514bf56
Daniel Chalmers <MoonDPC@gmail.com>
2016-02-09 21:28:40 -0500

    Add "Play Sound" action

---
+++ DesktopWidgets/Actions/PlaySoundAction.cs
@@ -0,0 +1,15 @@
+﻿using DesktopWidgets.Stores;
+
+namespace DesktopWidgets.Actions
+{
+    internal class PlaySoundAction : IAction
+    {
+        public string Path { get; set; }
+        public double Volume { get; set; } = 1.0;
+
+        public void Execute()
+        {
+            MediaPlayerStore.PlaySoundAsync(Path, Volume);
+        }
+    }
+}
