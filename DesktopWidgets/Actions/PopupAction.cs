d1eede448bcfda3335c18fbaf2a1ae3ad5a3bb22
Daniel Chalmers <MoonDPC@gmail.com>
2016-02-22 18:59:18 -0500

    Fix "Popup" action "Text", "Title" default values

--- DesktopWidgets/Actions/PopupAction.cs
+++ DesktopWidgets/Actions/PopupAction.cs
@@ -6,10 +6,10 @@ namespace DesktopWidgets.Actions
     internal class PopupAction : ActionBase
     {
         [DisplayName("Text")]
-        public string Text { get; set; }
+        public string Text { get; set; } = "";
 
         [DisplayName("Title")]
-        public string Title { get; set; }
+        public string Title { get; set; } = "";
 
         [DisplayName("Image")]
         public MessageBoxImage Image { get; set; }
