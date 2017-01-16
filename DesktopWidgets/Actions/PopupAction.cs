579bdb0ee5287f404ab12a1035f2a47cb73d3ee0
Daniel Chalmers <MoonDPC@gmail.com>
2017-01-15 22:36:37 -0500

    Add "Input Mode", "File" options to "Popup" action

--- DesktopWidgets/Actions/PopupAction.cs
+++ DesktopWidgets/Actions/PopupAction.cs
@@ -1,12 +1,19 @@
 ﻿using System.ComponentModel;
+using System.IO;
 using System.Windows;
+using DesktopWidgets.Classes;
 
 namespace DesktopWidgets.Actions
 {
     internal class PopupAction : ActionBase
     {
+        public FilePath FilePath { get; set; } = new FilePath();
+
         [DisplayName("Text")]
         public string Text { get; set; } = "";
+
+        [DisplayName("Input Mode")]
+        public InputMode InputMode { get; set; } = InputMode.Text;
 
         [DisplayName("Title")]
         public string Title { get; set; } = "";
@@ -17,7 +24,20 @@ namespace DesktopWidgets.Actions
         protected override void ExecuteAction()
         {
             base.ExecuteAction();
-            MessageBox.Show(Text, Title, MessageBoxButton.OK, Image);
+            var input = string.Empty;
+            switch (InputMode)
+            {
+                case InputMode.Clipboard:
+                    input = Clipboard.GetText();
+                    break;
+                case InputMode.File:
+                    input = File.ReadAllText(FilePath.Path);
+                    break;
+                case InputMode.Text:
+                    input = Text;
+                    break;
+            }
+            MessageBox.Show(input, Title, MessageBoxButton.OK, Image);
         }
     }
 }
