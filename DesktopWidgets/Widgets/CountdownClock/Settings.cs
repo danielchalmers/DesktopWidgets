535d5853aa85e72e8a3f9fdc5a4180dc41a39f9c
Daniel Chalmers <MoonDPC@gmail.com>
2017-02-24 22:48:18 -0500

    Change "Countdown" "End Sync" properties order

--- DesktopWidgets/Widgets/CountdownClock/Settings.cs
+++ DesktopWidgets/Widgets/CountdownClock/Settings.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using DesktopWidgets.WidgetBase.Settings;
+using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
 
 namespace DesktopWidgets.Widgets.CountdownClock
 {
@@ -24,26 +25,32 @@ namespace DesktopWidgets.Widgets.CountdownClock
         [DisplayName("Continue Counting")]
         public bool EndContinueCounting { get; set; } = false;
 
+        [PropertyOrder(0)]
         [Category("End Sync")]
         [DisplayName("Sync Next Year")]
         public bool SyncYear { get; set; } = false;
 
+        [PropertyOrder(1)]
         [Category("End Sync")]
         [DisplayName("Sync Next Month")]
         public bool SyncMonth { get; set; } = false;
 
+        [PropertyOrder(2)]
         [Category("End Sync")]
         [DisplayName("Sync Next Day")]
         public bool SyncDay { get; set; } = false;
 
+        [PropertyOrder(3)]
         [Category("End Sync")]
         [DisplayName("Sync Next Hour")]
         public bool SyncHour { get; set; } = false;
 
+        [PropertyOrder(4)]
         [Category("End Sync")]
         [DisplayName("Sync Next Minute")]
         public bool SyncMinute { get; set; } = false;
 
+        [PropertyOrder(5)]
         [Category("End Sync")]
         [DisplayName("Sync Next Second")]
         public bool SyncSecond { get; set; } = false;
