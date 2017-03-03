7fa863b241212f4472cb5847ef6f2a51dfec97f0
Daniel Chalmers <MoonDPC@gmail.com>
2017-03-03 16:35:03 -0500

    Add library credits to "About" text

--- DesktopWidgets/Helpers/AboutHelper.cs
+++ DesktopWidgets/Helpers/AboutHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using DesktopWidgets.Classes;
 using DesktopWidgets.Properties;
 
@@ -20,6 +22,13 @@ namespace DesktopWidgets.Helpers
                 stringBuilder.AppendLine();
                 stringBuilder.AppendLine($"Icon made by {IconCredits}");
                 stringBuilder.AppendLine();
+                stringBuilder.AppendLine("Libraries:");
+                foreach (var library in Libraries
+                    .Select(x => $"  {x.Key}: {x.Value}"))
+                {
+                    stringBuilder.AppendLine(library);
+                }
+                stringBuilder.AppendLine();
                 stringBuilder.Append(AssemblyInfo.Copyright);
 
                 return stringBuilder.ToString();
@@ -29,5 +38,16 @@ namespace DesktopWidgets.Helpers
         private static string IconCredits { get; } =
             "Freepik (http://www.freepik.com) from www.flaticon.com" +
             " is licensed under CC BY 3.0 (http://creativecommons.org/licenses/by/3.0/)";
+
+        private static Dictionary<string, string> Libraries => new Dictionary<string, string>
+        {
+            {"Common Service Locator", "commonservicelocator.codeplex.com"},
+            {"Extended WPF Toolkit", "wpftoolkit.codeplex.com"},
+            {"NotifyIcon", "hardcodet.net/projects/wpf-notifyicon"},
+            {"MVVM Light", "galasoft.ch/mvvm"},
+            {"Json.NET", "newtonsoft.com/json"},
+            {"NHotkey", "github.com/thomaslevesque/NHotkey"},
+            {"WpfAppBar", "github.com/PhilipRieck/WpfAppBar"}
+        };
     }
 }
