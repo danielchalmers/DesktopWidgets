9b87ebb21284942a26f2cd43b73ed0b9ee9a7c2d
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-10-03 18:11:40 -0400

    Expand property grid properties by default

---
+++ DesktopWidgets/Controls/PropertyGrid.cs
@@ -0,0 +1,11 @@
+﻿namespace DesktopWidgets.Controls
+{
+    public class PropertyGrid : Xceed.Wpf.Toolkit.PropertyGrid.PropertyGrid
+    {
+        protected override void OnSelectedObjectChanged(object oldValue, object newValue)
+        {
+            base.OnSelectedObjectChanged(oldValue, newValue);
+            ExpandAllProperties();
+        }
+    }
+}
