42744b2dc30448606ca656311ab6adcef6218d07
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-12-16 20:52:01 -0500

    Refactor Network Monitor bytes to text

--- DesktopWidgets/Widgets/LatencyMonitor/ViewModel.cs
+++ DesktopWidgets/Widgets/LatencyMonitor/ViewModel.cs
@@ -133,12 +133,12 @@ namespace DesktopWidgets.Widgets.LatencyMonitor
             if (Settings.ShowDownloadUsage)
             {
                 stringBuilder.Append(
-                    $" {StringHelper.BytesToString(downloadUsage, Settings.BandwidthDecimalPlaces).PadLeft(Settings.BandwidthPadding, ' ')}");
+                    $" {BytesToReadableString(downloadUsage, Settings.BandwidthDecimalPlaces).PadLeft(Settings.BandwidthPadding, ' ')}");
             }
             if (Settings.ShowUploadUsage)
             {
                 stringBuilder.Append(
-                    $" {StringHelper.BytesToString(uploadUsage, Settings.BandwidthDecimalPlaces).PadLeft(Settings.BandwidthPadding, ' ')}");
+                    $" {BytesToReadableString(uploadUsage, Settings.BandwidthDecimalPlaces).PadLeft(Settings.BandwidthPadding, ' ')}");
             }
             stringBuilder.Append($" {reply.RoundtripTime.ToString().PadLeft(Settings.LatencyPadding, '0')}ms");
             if (Settings.ShowStatus)
@@ -164,6 +164,57 @@ namespace DesktopWidgets.Widgets.LatencyMonitor
         {
             base.OnClose();
             _scanLatency = false;
+        }
+
+        // https://stackoverflow.com/a/11124118
+        private static string BytesToReadableString(long bytes, int decimalPlaces)
+        {
+            if (bytes <= 0)
+            {
+                return "0B";
+            }
+
+            string suffix;
+            double readable;
+            if (bytes >= 0x1000000000000000) // Exabyte.
+            {
+                suffix = "EB";
+                readable = (bytes >> 50);
+            }
+            else if (bytes >= 0x4000000000000) // Petabyte.
+            {
+                suffix = "PB";
+                readable = (bytes >> 40);
+            }
+            else if (bytes >= 0x10000000000) // Terabyte.
+            {
+                suffix = "TB";
+                readable = (bytes >> 30);
+            }
+            else if (bytes >= 0x40000000) // Gigabyte.
+            {
+                suffix = "GB";
+                readable = (bytes >> 20);
+            }
+            else if (bytes >= 0x100000) // Megabyte.
+            {
+                suffix = "MB";
+                readable = (bytes >> 10);
+            }
+            else if (bytes >= 0x400) // Kilobyte.
+            {
+                suffix = "KB";
+                readable = bytes;
+            }
+            else
+            {
+                return bytes.ToString("0B"); // Byte.
+            }
+
+            // Divide by 1024 to get fractional value.
+            readable = Math.Round(readable / 1024, decimalPlaces);
+
+            return readable.ToString("0") + suffix;
         }
     }
 }
